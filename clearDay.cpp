d0cf4e467e95b78fb97890ac89a811f355918300
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-12-11 23:35:52 +0600

    Timus-1000 Problem
    Timus-1000 Problem

---
+++ clearDay.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y, clearDay;
+
+    cin >> x >> y;
+
+    clearDay = 7 - (x + y);
+
+    cout << clearDay << "\n";
+
+    return 0;
+}
