a5f9c0a0e2f499324380d47d6920c1500f6882d2
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-12-12 00:17:37 +0600

    Double Rent Codechef Problem
    Double Rent Codechef Problem

---
+++ Data_Types/doubleRent.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int initialRent, finalrent;
+
+    cin >> initialRent;
+
+    finalrent = 2 * initialRent;
+
+    cout << finalrent << "\n";
+
+    return 0;
+}
