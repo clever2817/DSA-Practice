bb15a6cb1cbe1b7485adaff085d90afd886dd640
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-12-11 23:16:05 +0600

    ClearDay_(CodeChef) Problem
    ClearDay_(CodeChef) Problem

---
+++ clearDay.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y, cloudy;
+
+    cin >> x >> y;
+
+    cloudy = 7 - (x + y);
+
+    cout << cloudy << "\n";
+
+    return 0;
+}
