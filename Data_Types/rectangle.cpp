992d45df0131ae1e83546bdeb306603192f2396a
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-12-12 21:42:45 +0600

    Rectangle (AIZU) problem
    Rectangle (AIZU) problem

---
+++ Data_Types/rectangle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    int area = a * b;
+
+    int perimeter = 2 * (a + b);
+
+    cout << area << " " << perimeter << "\n";
+
+    return 0;
+}
