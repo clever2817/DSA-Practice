d9a8d95d82cadab3a9fcc484586c7b1e197d39a1
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-12-25 21:36:20 +0600

    foodCost Problem (CodeChef)
    foodCost Problem (CodeChef)

---
+++ Data_Types/XCubic.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    int result = x * x * x;
+
+    cout << result << "\n";
+
+    return 0;
+}
---
+++ Data_Types/foodCost.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x >> y;
+
+    x*=6;
+
+    int ans = x + y;
+
+    cout << ans << "\n";
+
+    return 0;
+}
